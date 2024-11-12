afadc0cc90edfba390129c1f9e1b99af308d30a9
Slawomir Kwasniak <slawomir.kwasniak@intel.com>
2024-11-12 14:35:25 +0100

    Test quality never above 50

--- python/test_gilded_rose.py
+++ python/test_gilded_rose.py
@@ -7,25 +7,52 @@ from gilded_rose import Item, GildedRose
 class GildedRoseTest(unittest.TestCase):
     def test_regular_item(self):
         item_type = "foo"
-        items = [Item(item_type, 24, 42)]
+        items = [Item(item_type, sell_in=24, quality=42)]
         gilded_rose = GildedRose(items)
         gilded_rose.update_quality()
         self.assertEqual(item_type, items[0].name)
         self.assertEqual(41, items[0].quality)
         self.assertEqual(23, items[0].sell_in)
 
+    def test_item_after_sell_in(self):
+        item_type = "foo"
+        items = [Item(item_type, sell_in=0, quality=8)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEqual(item_type, items[0].name)
+        self.assertEqual(6, items[0].quality)
+        self.assertEqual(-1, items[0].sell_in)
+
+    def test_item_quality_never_negative(self):
+        item_type = "foo"
+        items = [Item(item_type, sell_in=0, quality=0)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEqual(item_type, items[0].name)
+        self.assertEqual(0, items[0].quality)
+        self.assertEqual(-1, items[0].sell_in)
+
     def test_aged_brie_item(self):
         item_type = "Aged Brie"
-        items = [Item(item_type, 24, 42)]
+        items = [Item(item_type, sell_in=24, quality=42)]
         gilded_rose = GildedRose(items)
         gilded_rose.update_quality()
         self.assertEqual(item_type, items[0].name)
         self.assertEqual(43, items[0].quality)
         self.assertEqual(23, items[0].sell_in)
 
+    def test_quality_never_above_50(self):
+        item_type = "Aged Brie"
+        items = [Item(item_type, sell_in=24, quality=50)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEqual(item_type, items[0].name)
+        self.assertEqual(50, items[0].quality)
+        self.assertEqual(23, items[0].sell_in)
+
     def test_sulfuras_item(self):
         item_type = "Sulfuras, Hand of Ragnaros"
-        items = [Item(item_type, 24, 42)]
+        items = [Item(item_type, sell_in=24, quality=42)]
         gilded_rose = GildedRose(items)
         gilded_rose.update_quality()
         self.assertEqual(item_type, items[0].name)
@@ -34,7 +61,7 @@ class GildedRoseTest(unittest.TestCase):
 
     def test_back_stage_ticket(self):
         item_type = "Backstage passes to a TAFKAL80ETC concert"
-        items = [Item(item_type, 24, 42)]
+        items = [Item(item_type, sell_in=24, quality=42)]
         gilded_rose = GildedRose(items)
         gilded_rose.update_quality()
         self.assertEqual(item_type, items[0].name)
