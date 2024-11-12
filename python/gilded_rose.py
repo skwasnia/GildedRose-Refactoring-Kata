59589af336db12f3549a79e39030d1c694e0b67e
Slawomir Kwasniak <slawomir.kwasniak@intel.com>
2024-11-12 15:00:20 +0100

    Handle regular item

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -22,6 +22,13 @@ def _handle_backstage_pass(item):
 def _handle_sulfuras(item):
     pass
 
+def _handle_regular_item(item):
+    if item.quality > 0:
+        item.quality -= 1
+    item.sell_in -= 1
+    if item.sell_in < 0 and item.quality > 0:
+        item.quality -= 1
+
 class GildedRose(object):
 
     def __init__(self, items):
@@ -38,35 +45,9 @@ class GildedRose(object):
             elif item.name == "Sulfuras, Hand of Ragnaros":
                 _handle_sulfuras(item)
                 return
-
-            if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert":
-                if item.quality > 0:
-                    if item.name != "Sulfuras, Hand of Ragnaros":
-                        item.quality = item.quality - 1
             else:
-                if item.quality < 50:
-                    item.quality = item.quality + 1
-                    if item.name == "Backstage passes to a TAFKAL80ETC concert":
-                        if item.sell_in < 11:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-                        if item.sell_in < 6:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-            if item.name != "Sulfuras, Hand of Ragnaros":
-                item.sell_in = item.sell_in - 1
-            if item.sell_in < 0:
-                if item.name != "Aged Brie":
-                    if item.name != "Backstage passes to a TAFKAL80ETC concert":
-                        if item.quality > 0:
-                            if item.name != "Sulfuras, Hand of Ragnaros":
-                                item.quality = item.quality - 1
-                    else:
-                        item.quality = item.quality - item.quality
-                else:
-                    if item.quality < 50:
-                        item.quality = item.quality + 1
-
+                _handle_regular_item(item)
+                return
 
 class Item:
     def __init__(self, name, sell_in, quality):
